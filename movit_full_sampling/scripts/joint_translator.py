fe3a769856b0182e61ad8c984eaca3dd4e7fad58
Gharablyy <sofagharably@gmail.com>
2025-08-22 16:19:46 +0300

    Finalizing handling topic data and sending ArmControl custom message

--- movit_full_sampling/scripts/joint_translator.py
+++ movit_full_sampling/scripts/joint_translator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float64, Float64MultiArray
+from std_msgs.msg import Float64, Bool ,Float64MultiArray
 from geometry_msgs.msg import PoseStamped
-
+from roar_msgs.msg import ArmControl # type: ignore
+import math
 class JointCommandSplitter:
     """
     A ROS node that subscribes to a single Float64MultiArray message
@@ -28,6 +29,18 @@ class JointCommandSplitter:
             "/joint_lee_position_controller/command"
         ]
 
+        self.last_joint_rad = {
+            'joint1': 0.0,
+            'joint2': 0.0,
+            'joint3': 0.0,
+            'joint4': 0.0,
+            'joint5': 0.0,
+            'end_effector': 0.0
+        }
+
+        self.regolith_flag = False
+        self.rock_flag = False
+        
         # A list to hold the publisher objects
         self.publishers = []
         for topic_name in self.output_topics:
@@ -35,65 +48,74 @@ class JointCommandSplitter:
             self.publishers.append(pub)
             rospy.loginfo("Created publisher for topic: %s", topic_name)
 
+        # --- CHANGE 2: Create a new publisher for your custom ArmControl message ---
+        self.arm_control_pub = rospy.Publisher('/arm_control_values', ArmControl, queue_size=10)
+        rospy.loginfo("Created publisher for topic: /arm_control_values")
+
         # Create a subscriber to listen for the Float64MultiArray messages
-        rospy.Subscriber("/robot/joint_command", PoseStamped, self.callback)
+        rospy.Subscriber("/robot/joint_command", PoseStamped, self.joint_callback)
         rospy.loginfo("Subscribed to topic: /robot/joint_command")
 
-    def callback(self, msgInc:PoseStamped):
+        rospy.Subscriber("/robot/rock_storage", Bool, self.rock_callback)
+        rospy.loginfo("Subscribed to topic: /robot/rock_storage")
+
+        rospy.Subscriber("/robot/regolith_storage", Bool, self.regolith_callback)
+        rospy.loginfo("Subscribed to topic: /robot/regolith_storage")
+
+    def rock_callback(self, msg:Bool):
+        """
+        Callback function for the rock storage topic.
+        Updates the rock_flag based on the received message.
+        """
+        self.rock_flag = msg.data
+        rospy.loginfo("Rock storage flag updated: %s", self.rock_flag)
+        self.publish_state()
+
+    def regolith_callback(self, msg:Bool):
+        """
+        Callback function for the regolith storage topic.
+        Updates the regolith_flag based on the received message.
+        """
+        self.regolith_flag = msg.data
+        rospy.loginfo("Regolith storage flag updated: %s", self.regolith_flag)
+        self.publish_state()
+
+    def publish_state(self):
+        arm_control_msg = ArmControl()
+        arm_control_msg.header.stamp = rospy.Time.now()
+        arm_control_msg.header.frame_id = "base_link"
+        arm_control_msg.joint1 = self.last_joint_rad['joint1']
+        arm_control_msg.joint2 = self.last_joint_rad['joint2']
+        arm_control_msg.joint3 = self.last_joint_rad['joint3']
+        arm_control_msg.joint4 = self.last_joint_rad['joint4']
+        arm_control_msg.joint5 = self.last_joint_rad['joint5']
+        arm_control_msg.end_effector = self.last_joint_rad['end_effector']      
+        arm_control_msg.regolith = self.regolith_flag
+        arm_control_msg.rock = self.rock_flag
+        self.arm_control_pub.publish(arm_control_msg)
+
+    def joint_callback(self, msg_in:PoseStamped):
         """
         Callback function that processes the incoming Float64MultiArray message.
         It splits the array and publishes each element to the corresponding joint topic.
         """
-        # rospy.loginfo("Received a new Float64MultiArray message.")
 
-        # Check if the number of received data elements matches the expected number
-        # if len(msg.data) != len(self.publishers):
-        #     rospy.logwarn(
-        #         "Received array size (%d) does not match the number of publishers (%d). Skipping.",
-        #         len(msg.data), len(self.publishers)
-        #     )
-        #     return
+        # update the joint angles in the ArmControl message
+        self.last_joint_rad['joint1'] = msg_in.pose.position.x / 180.0 * math.pi
+        self.last_joint_rad['joint2'] = msg_in.pose.position.y / 180.0 * math.pi
+        self.last_joint_rad['joint3'] = msg_in.pose.position.z / 180.0 * math.pi
+        self.last_joint_rad['joint4'] = msg_in.pose.orientation.x / 180.0 * math.pi
+        self.last_joint_rad['joint5'] = msg_in.pose.orientation.y / 180.0 * math.pi
+        self.last_joint_rad['end_effector'] = msg_in.pose.orientation.z
 
+        self.publishers[0].publish(Float64(self.last_joint_rad['joint1']))
+        self.publishers[1].publish(Float64(self.last_joint_rad['joint2']))
+        self.publishers[2].publish(Float64(self.last_joint_rad['joint3']))
+        self.publishers[3].publish(Float64(self.last_joint_rad['joint4']))
+        self.publishers[4].publish(Float64(self.last_joint_rad['joint5']))
+        self.publishers[5].publish(Float64(self.last_joint_rad['end_effector']))
+        self.publish_state()
 
-        # # Iterate through the received data and the publishers
-        # for i in range(len(self.publishers)- 2) :
-        #     # Create a new Float64 message with the data from the array
-        #     joint_value = Float64()
-        #     joint_value.data = msg.data[i]/180.0 * 3.14159
-
-        #     # Publish the new message to the corresponding topic
-        #     self.publishers[i].publish(joint_value)
-            # rospy.loginfo("Published data[%d]: %.2f to topic %s", i, joint_value.data, self.output_topics[i])
-        # rospy.loginfo("WENT HEREE")
-        msg = Float64()
-        msg.data = msgInc.pose.position.x/180.0 * 3.14159
-        self.publishers[0].publish(msg)
-        msg = Float64()
-        msg.data = msgInc.pose.position.y/180.0 * 3.14159
-        self.publishers[1].publish(msg)
-        msg = Float64()
-        msg.data = msgInc.pose.position.z/180.0 * 3.14159
-        self.publishers[2].publish(msg)
-        msg = Float64()
-        msg.data = msgInc.pose.orientation.x/180.0 * 3.14159
-        self.publishers[3].publish(msg)
-        msg = Float64()
-        msg.data = msgInc.pose.orientation.y/180.0 * 3.14159
-        self.publishers[4].publish(msg)
-        msg = Float64()
-        msg.data = msgInc.pose.orientation.z
-        self.publishers[5].publish(msg)
-        msg = Float64()
-        msg.data = -msgInc.pose.orientation.z
-        self.publishers[6].publish(msg)
-
-        # joint_value = Float64()
-        # joint_value.data = msg.data[5]
-        # self.publishers[5].publish(joint_value)
-        
-        # joint_value = Float64()
-        # joint_value.data = -msg.data[5]
-        # self.publishers[6].publish(joint_value)
 
 def main():
     """
